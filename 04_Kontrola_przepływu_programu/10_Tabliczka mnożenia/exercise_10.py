9cd290abdd771e0c3d249a6985823d1a11c1838b
xg04tx <kozi98025@gmail.com>
2020-04-14 17:54:09 +0200

    koniec

--- 04_Kontrola_przepływu_programu/10_Tabliczka mnożenia/exercise_10.py
+++ 04_Kontrola_przepływu_programu/10_Tabliczka mnożenia/exercise_10.py
@@ -1,3 +1,3 @@
 n = int(input('Podaj liczbę od 1 do 10: '))
 for i in range(1, 11, ):
-    print(n, 'x', i, '=', n*i)
+    print(n, 'x', i, '=', n * i)
---
+++ 05_Styl_kodu/styl_kodu.py
@@ -0,0 +1,3 @@
+# Kod który napisałem w ćwiczeniach jest w miarę zgodny z PEP 8
+# Nie uniknąłem zgrzytów w postaci mało rozpoznawalnych nazw, i odstępów w działaniach
+# np. c=i+1 zamiast c = i + 1
